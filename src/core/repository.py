dcb409bde8728461656fa8e658f7c2489e2cf077
devzelix <devzelix@gmail.com>
2025-03-26 02:20:05 -0400

    Se tradujo el codigo a ingles de 'repositorio.py' para buenas practicas

---
+++ src/core/repository.py
@@ -0,0 +1,28 @@
+from colorama import Fore
+from pathlib import Path
+
+class Repository:
+    def __init__(self):
+        self.repository_path = Path(".") / ".solo-git/"
+        self.objects_path = self.repository_path / "objetos/"
+        self.references_path = self.repository_path / "referencias/"
+        self.heads_path = self.references_path / "cabezas/"
+        self.logs_path = self.repository_path / "registros/"
+        self.head_path = self.repository_path / "CABEZA"
+        self.configuration_path = self.repository_path / "configuracion"
+
+    def inicializar(self):
+        try:
+            if self.repository_path.exists():
+                print(Fore.YELLOW + f"\nRepositorio solo-git ya inicializado en: {self.repository_path.resolve()}\n")
+            else:
+                self.repository_path.mkdir()
+                self.objects_path.mkdir()
+                self.references_path.mkdir()
+                self.heads_path.mkdir()
+                self.logs_path.mkdir()
+                self.head_path.write_text(f"referencia: {self.heads_path}/main")
+                self.configuration_path.write_text("")
+                print(Fore.GREEN + f"\nRepositorio solo-git vacío inicializado en: {self.repository_path.resolve()}\n")
+        except Exception as e:
+            print(Fore.RED + f"\nHubo un error al incializar el repositorio: {e}\n")
